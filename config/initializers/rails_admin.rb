cee50a121a40b3540f0c6aa5897804a3482fb4a9
johnson-darko <johnsondarko365@gmail.com>
2022-08-14 22:53:21 +0200

    remove admin restriction

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,8 +1,5 @@
 RailsAdmin.config do |config|
-  config.asset_source = :importmap
-  config.authorize_with do |controller|
-    redirect_to main_app.root_path unless current_user && current_user.admin
-  end
+ 
   ### Popular gems integration
 
   ## == Devise ==
